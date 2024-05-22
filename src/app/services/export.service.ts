6f35da92bd1340a926b73dad0402841e4d881b10
Christian Aromin <caromin19@gmail.com>
2024-05-22 04:04:30 -0400

    downloadable link

--- src/app/services/export.service.ts
+++ src/app/services/export.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { UserInfoService } from './user-info.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExportService {
 
-  constructor() { }
+  constructor(protected userInfo: UserInfoService) { }
 
   exportCV() {
-    console.log('export');
+    const link = document.createElement('a');
+    link.href = 'assets/resume.pdf';
+    link.download = `${this.userInfo.firstName}_${this.userInfo.lastName}_Resume.pdf`;
+    link.click();
   }
 }
